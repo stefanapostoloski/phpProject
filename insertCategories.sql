a261ee1651966d031775d6595286251a3852e831
Stefan <stefan.apostoloski@gmail.com>
2021-03-09 16:01:25 +0100

    sql for insertion of categories in user_group table in db

---
+++ insertCategories.sql
@@ -0,0 +1,27 @@
+INSERT INTO user_group (name, parent_group) values ("Front End Developer",null);
+INSERT INTO user_group (name, parent_group) values ("Back End Developer",null);
+
+
+INSERT INTO user_group (name, parent_group) values ("Angular",1);
+INSERT INTO user_group (name, parent_group) values ("React",1);
+INSERT INTO user_group (name, parent_group) values ("Vue",1);
+
+INSERT INTO user_group (name, parent_group) values ("PHP",2);
+INSERT INTO user_group (name, parent_group) values ("NodeJS",2);
+
+
+INSERT INTO user_group (name, parent_group) values ("AngularJS",3);
+INSERT INTO user_group (name, parent_group) values ("Angular 2",3);
+
+INSERT INTO user_group (name, parent_group) values ("React native",4);
+
+
+INSERT INTO user_group (name, parent_group) values ("Symfony",6);
+INSERT INTO user_group (name, parent_group) values ("Laravel",6);
+
+INSERT INTO user_group (name, parent_group) values ("Express",7);
+INSERT INTO user_group (name, parent_group) values ("NestJS",7);
+
+INSERT INTO user_group (name, parent_group) values ("Silex",11);
+
+INSERT INTO user_group (name, parent_group) values ("Lumen",12);
